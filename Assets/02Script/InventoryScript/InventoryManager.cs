40846f42283403fec2b8bd936245d2fc403294a4
JJO <143975556+CHOJINOH@users.noreply.github.com>
2025-05-29 12:27:03 +0900

    invenManager업데이트

--- Assets/02Script/InventoryScript/InventoryManager.cs
+++ Assets/02Script/InventoryScript/InventoryManager.cs
@@ -1,23 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance { get; private set; }
 
     [Header("인벤토리 데이터")]
-    public InventoryData data;    // Inspector에서 드래그&드롭
+    public InventoryData data;
 
     void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
+        if (Instance == null) Instance = this;
+        else { Destroy(gameObject); return; }
 
         if (data == null)
             Debug.LogWarning("InventoryManager.data에 인벤토리 데이터가 할당되지 않았습니다!");
@@ -27,27 +21,44 @@ public class InventoryManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            AddSwordToInventory();
+            var item = LoadItemByName("강철 검");
+            if (item != null)
+            {
+                data.storageItems.Add(item);
+                InventoryUIManager.Instance?.RefreshAll();
+            }
         }
     }
 
-    void AddSwordToInventory()
+    ItemData LoadItemByName(string name)
     {
-        if (data == null) return;
-
-        var sword = new ItemData
+        TextAsset jsonFile = Resources.Load<TextAsset>("Data/Items");
+        if (jsonFile == null)
         {
-            itemName = "강철 검",
-            description = "단단한 철로 만들어진 검입니다.",
-            type = EquipmentType.Weapon,
-            icon = Resources.Load<Sprite>("Icons/IronSword") // Resources 폴더에 있어야 함
-        };
+            Debug.LogError("Items.json 파일을 찾을 수 없습니다.");
+            return null;
+        }
 
-        data.storageItems.Add(sword); // 인벤토리 보관함에 추가만 함
+        string wrapped = "{\"items\":" + jsonFile.text + "}";
+        ItemListWrapper wrapper = JsonUtility.FromJson<ItemListWrapper>(wrapped);
+        if (wrapper?.items == null) return null;
 
-        // UI 갱신
-        InventoryUIManager.Instance?.RefreshAll();
+        foreach (var item in wrapper.items)
+        {
+            if (item.itemName == name)
+            {
+                item.icon = Resources.Load<Sprite>(item.iconPath);
+                return item;
+            }
+        }
+
+        Debug.LogWarning($"{name} 아이템을 찾을 수 없습니다.");
+        return null;
     }
 
-
+    [System.Serializable]
+    private class ItemListWrapper
+    {
+        public List<ItemData> items;
+    }
 }
