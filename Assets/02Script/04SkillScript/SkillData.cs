6e8993d2cd2aec3a2e2ed91f1d384079f08bbb45
add9010 <101711754+add9010@users.noreply.github.com>
2026-03-27 16:01:52 +0900

    Fix formatting of cooldown variable in SkillData

--- Assets/02Script/04SkillScript/SkillData.cs
+++ Assets/02Script/04SkillScript/SkillData.cs
@@ -8,7 +8,7 @@ public class SkillData : ScriptableObject
     public GameObject skillLogicPrefab; // 여기서 Skill1 프리팹 연결
 
     [Header("스킬 쿨타임")]
-    public float cooldown = 1.0f; // 스킬마다 개별 설정 가능
+    public float cooldown = 1.0f; 
     public int manaCost = 1; // 추가
 
 
