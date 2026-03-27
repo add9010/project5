a6d632d67fb7786bee8d74cef068affd7a71393c
add9010 <101711754+add9010@users.noreply.github.com>
2026-03-27 16:10:19 +0900

    Remove comments from NetworkCombatManager

--- Assets/02Script/CombatManager/NetworkCombatManager.cs
+++ Assets/02Script/CombatManager/NetworkCombatManager.cs
@@ -2,16 +2,13 @@ using System.Diagnostics;
 
 public static class NetworkCombatManager
 {
-    // Player 객체 참조를 저장할 정적 변수
     private static Player _currentPlayer;
 
-    // Player 참조 설정 메서드
     public static void Initialize(Player player)
     {
         _currentPlayer = player;
     }
 
-    // 플레이어가 몬스터에게 데미지를 입힐 때 호출
     public static void SendMonsterDamage(int damage)
     {
        
@@ -19,7 +16,6 @@ public static class NetworkCombatManager
         _currentPlayer.SendMonsterDamage(damage);
     }
 
-    // 플레이어가 트랩에게 데미지를 입힐 때 호출
     public static void SendTrapDamage(string trapId, int damage)
     {
        
